96016866ac9bb0337ec1a6abfafbbd599e077461
A F <18417689+perplexhub@users.noreply.github.com>
2020-03-19 13:17:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,11 @@ function runAction() {
 	// Deploy to Nexus
 	// The "deploy" profile is used in case the user wants to perform certain steps only during
 	// deployment and not in the install phase
+	const gitUsername = getInput("git_username", true);
+	const gitEmail = getInput("git_email", true);
 	log("Releasing the Maven project");
+	run(`git config --global user.email "${gitEmail}"`);
+	run(`git config --global user.name "${gitUsername}"`);
 	run(
 		`mvn release:clean release:prepare --batch-mode --activate-profiles ${activateProfiles} --settings ${mavenSettingsPath} ${mavenArgs}`,
 		getInput("directory") || null,
